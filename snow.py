63f1995aa99eab57f49f27a120fbc9d663390147
Andy <89641810+Andy00L@users.noreply.github.com>
2023-12-26 21:17:53 -0500

    Update snow.py
    This is the entire, working code

--- snow.py
+++ snow.py
@@ -3,14 +3,59 @@ import os
 import time
 import random
 
-snow_density = 5
-delay = 0.3
+# Snow density
+snow_density = 7
+delay = 0.2
 
-snowflake = ["❄️","❅","❃","❋","❉"]
+# Snowflake icons
+snowflake = ["❄️", "❅", "❃", "❋", "❉"]
 
+# Get screen size
 term = os.get_terminal_size()
 
+# Screen width and height
 width = term.columns
 height = term.lines
 
-print(width, height)
+# Create a grid with the height and width of the screen
+grid = []
+for _ in range(height):
+    grid.append([' '] * width)
+
+# Function to draw the grid
+def draw_grid():
+    # Clear the screen
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    # Hide the cursor
+    print('\033[?25l')
+
+    # Output for each row of the grid
+    output = ''
+    for row in grid:
+        output += ''.join(row) + '\n'
+    output = output.strip('\n')
+    print(output, end='')
+
+# Draw the initial grid
+draw_grid()
+
+# Infinite loop to generate snowflakes
+while True:
+    # Create a new row of snowflakes
+    row = []
+    for _ in range(width):
+        if random.random() < snow_density / 100:
+            row.append(random.choice(snowflake))
+        else:
+            row.append(' ')
+
+    # Add the new row to the grid and remove the last row
+    grid.insert(0, row)
+    grid.pop()
+
+    # Draw the updated grid
+    draw_grid()
+
+    # Wait for a bit before generating the next snowflakes
+    time.sleep(delay)
